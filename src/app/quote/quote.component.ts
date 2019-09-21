668208f94acba3e0f4a567f25fa77b8d6ea07d77
IKERRIZ <okothfaith94@gmail.com>
2019-09-22 00:47:30 +0300

    Moved the quote array permanently from app to quote component class

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{ Quote } from'../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes :Quote[] = [
+    {id:1, name:'The eyes are the windows of the soul'},
+    {id:2,name:'Stop and smell the roses'},
+    {id:3,name:'Everything has beauty, but not everyone sees it'},
+    {id:4,name:'Real beauty is to be true to oneself'},
+    {id:5,name:'Beauty is power; a smile is its sword'},
+    {id:6,name:'The best part of beauty is that which no picture can express'},
+  ];
 
   constructor() { }
 
