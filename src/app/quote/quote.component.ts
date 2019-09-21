42cfaeb7a4ee075a2f776dc5fb1dabc673758f8a
IKERRIZ <okothfaith94@gmail.com>
2019-09-22 01:20:18 +0300

    Addchild components

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import{ Quote } from'../quote';
 export class QuoteComponent implements OnInit {
 
   quotes :Quote[] = [
-    {id:1, name:'The eyes are the windows of the soul'},
-    {id:2,name:'Stop and smell the roses'},
-    {id:3,name:'Everything has beauty, but not everyone sees it'},
-    {id:4,name:'Real beauty is to be true to oneself'},
-    {id:5,name:'Beauty is power; a smile is its sword'},
-    {id:6,name:'The best part of beauty is that which no picture can express'},
+    {id:1, name:'The eyes are the windows of the soul',description:'What your soul speaks is displayed in your eyes'},
+    {id:2,name:'Stop and smell the roses',description:'At times when you feel overwhelmed first find solutions'},
+    {id:3,name:'Everything has beauty, but not everyone sees it',description:'Like in life most people have eyes but do not see'},
+    {id:4,name:'Real beauty is to be true to oneself',description:'Honesty allows us to be free'},
+    {id:5,name:'Beauty is power; a smile is its sword',description:'Expression of beauty very simple just wear a smile'},
+    {id:6,name:'The best part of beauty is that which no picture can express',description:'You do not have to see my physical appearance to judge how beautiful I am'},
   ];
 
   constructor() { }
