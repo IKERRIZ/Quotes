24659522517e81eee80548ae716b51bdab504040
IKERRIZ <okothfaith94@gmail.com>
2019-09-22 00:03:41 +0300

    Create several quotes using quote blueprint class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { EmptyError } from 'rxjs';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,14 @@ import { EmptyError } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes :string[];
+  quotes :Quote[] = [
+    {id:1, name:'The eyes are the windows of the soul'},
+    {id:2,name:'Stop and smell the roses'},
+    {id:3,name:'Everything has beauty, but not everyone sees it'},
+    {id:4,name:'Real beauty is to be true to oneself'},
+    {id:5,name:'Beauty is power; a smile is its sword'},
+    {id:6,name:'The best part of beauty is that which no picture can express'},
+  ];
 
   constructor(){
     this.quotes = ['The eyes are the windows of the soul', 'Stop and smell the roses', 'Everything has beauty, but not everyone sees it']
