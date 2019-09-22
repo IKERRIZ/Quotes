0da33fd687e7a993725d9f3e06626cd2990b935f
IKERRIZ <okothfaith94@gmail.com>
2019-09-22 09:17:30 +0300

    Addcomponent quote detail to receive quote description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,14 @@ import { Quote } from './quote';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes :Quote[] = [
-    {id:1, name:'The eyes are the windows of the soul'},
-    {id:2,name:'Stop and smell the roses'},
-    {id:3,name:'Everything has beauty, but not everyone sees it'},
-    {id:4,name:'Real beauty is to be true to oneself'},
-    {id:5,name:'Beauty is power; a smile is its sword'},
-    {id:6,name:'The best part of beauty is that which no picture can express'},
-  ];
+  // quotes :Quote[] = [
+  //   {id:1, name:'The eyes are the windows of the soul'},
+  //   {id:2,name:'Stop and smell the roses'},
+  //   {id:3,name:'Everything has beauty, but not everyone sees it'},
+  //   {id:4,name:'Real beauty is to be true to oneself'},
+  //   {id:5,name:'Beauty is power; a smile is its sword'},
+  //   {id:6,name:'The best part of beauty is that which no picture can express'},
+  // ];
 
 //   constructor(){
 //     this.quotes = ['The eyes are the windows of the soul', 'Stop and smell the roses', 'Everything has beauty, but not everyone sees it']
