912f8fe2a343b53bb73ca86ef5908dadc07d6fd9
IKERRIZ <okothfaith94@gmail.com>
2019-09-22 16:00:17 +0300

    Change quote so that we can put the constructors in use

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,17 +9,17 @@ import{ Quote } from'../quote';
 export class QuoteComponent implements OnInit {
 
   quotes :Quote[] = [
-    {id:1, name:'The eyes are the windows of the soul',description:'What your soul speaks is displayed in your eyes'},
-    {id:2,name:'Stop and smell the roses',description:'At times when you feel overwhelmed first find solutions'},
-    {id:3,name:'Everything has beauty, but not everyone sees it',description:'Like in life most people have eyes but do not see'},
-    {id:4,name:'Real beauty is to be true to oneself',description:'Honesty allows us to be free'},
-    {id:5,name:'Beauty is power; a smile is its sword',description:'Expression of beauty very simple just wear a smile'},
-    {id:6,name:'The best part of beauty is that which no picture can express',description:'You do not have to see my physical appearance to judge how beautiful I am'},
+    new Quote(1, 'The eyes are the windows of the soul','What your soul speaks is displayed in your eyes'),
+    new Quote(2,'Stop and smell the roses','At times when you feel overwhelmed first find solutions'),
+    new Quote(3,'Everything has beauty, but not everyone sees it','Like in life most people have eyes but do not see'),
+    new Quote(4,'Real beauty is to be true to oneself','Honesty allows us to be free'),
+    new Quote(5,'Beauty is power; a smile is its sword','Expression of beauty very simple just wear a smile'),
+    new Quote(6,'The best part of beauty is that which no picture can express','You do not have to see my physical appearance to judge how beautiful I am'),
   ];
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
   }
 
 }
